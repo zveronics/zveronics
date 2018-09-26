74416000dce358b2cbcbddf7eccc982d54600d3e
Borodin Gregory <grihabor@gmail.com>
2018-09-26 16:05:21 +0000

    Basic setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zveronics',
+    description='Python server for zveronics game',
+    author='Zveronics developers',
+    author_email='grihabor@gmail.com',
+    url='https://github.com/zveronics/zveronics',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+)
