230ec419761e2300f37ceceed8330902bb42d955
Borodin Gregory <grihabor@gmail.com>
2018-09-28 20:31:35 +0000

    Fix flake8 lints

--- tests/test_logging_config.py
+++ tests/test_logging_config.py
@@ -4,4 +4,3 @@ import pkg_resources
 def test_logging_config():
     assert pkg_resources.resource_exists('zveronics', 'etc/logging.yaml')
     assert not pkg_resources.resource_isdir('zveronics', 'etc/logging.yaml')
-
