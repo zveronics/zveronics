308bd033e2e332977d1f153a92d06a5fc6bf2ed1
Borodin Gregory <grihabor@gmail.com>
2018-09-29 13:03:37 +0000

    Configure pylint, fix pylint errors

--- src/zveronics/server.py
+++ src/zveronics/server.py
@@ -16,29 +16,21 @@ def pack(data):
     return msgpack.packb(data, use_bin_type=True)
 
 
-class MsgSize(struct.Struct):
-    def __init__(self):
-        super().__init__('<I')
-
-
 class UserRequestHandler(socketserver.BaseRequestHandler):
     """The request handler class for zveronics server."""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     def handle(self):
-        s = MsgSize()
-        size = int(s.size)
+        int_format = struct.Struct('<I')
+        size = int(int_format.size)
         log.debug('Waiting message size (%s bytes)', str(size))
-        msg_size = s.unpack(self.request.recv(size))[0]
+        msg_size = int_format.unpack(self.request.recv(size))[0]
         log.debug('Reading message of size %s', msg_size)
         msg = unpack(self.request.recv(msg_size))
         log.debug('Got message: %s', json.dumps(msg))
 
 
 def serve_zveronics(host, port):
-    log.info(f'Starting zveronics server on {host}:{port}')
+    log.info('Starting zveronics server on %s:%s', host, port)
     with socketserver.TCPServer((host, port),
                                 UserRequestHandler) as server:
         server.serve_forever()
